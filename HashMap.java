a175da25a4096b7275fb1a7c428b73894301ec41
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2023-10-09 21:42:01 +0530

    Add files via upload
    🌟 Key Takeaways:
    
    Demonstrates the usage of generics in Java.
    Shows how to create and manipulate a HashMap with specific key and value types.
    Reflects my commitment to improving my Java programming skills.
    🤝 Connect With Me:
    Let's connect and share our experiences in the world of coding. I'm always eager to learn from others and collaborate on exciting projects.
    
    Feel free to reach out if you have any questions, feedback, or if you'd like to discuss code, tech, or anything related. Looking forward to connecting with you! 🔗

---
+++ HashMap.java
@@ -0,0 +1,32 @@
+package hashing1;
+import java.util.*;
+
+public class HashMap {
+    public static void main(String[]args){
+       HashMap<String,String> hmap1 = new HashMap<String,String >();  
+
+        hmap1.put("jone",2345889);//here list will added in to the hasemap
+        hmap1.put("jay",2345723);
+        hmap1.put("bond",007);
+        hmap1.put("weak",2345984);
+        hmap1.put("defender",23435);
+         // in the hase map we can store the null key and null value 
+        hmap1.put(null,null);
+        //but we can not store more than one null "key ". but we can more than one null "value"
+        hmap1.put("Gopal",null);
+
+        System.out.println(hmap1);//it will print the hase map list 
+        System.out.println("the size of HasMap:" , hmap1.size());//it will print the number of the element in the list 
+ 
+        //like the above code we can define more than one hasemaps
+
+        if(hmap1.containskey("weak")){
+            Integer val=hmap1.get("weak");//it will store the value of the key 
+            System.out.println(+ val);//it will print the  value of the key 
+        }
+
+        
+      }
+
+    
+}
